6618a61b651d145886995e13a96a81d7ba4dcd60
Shadow <kcoudray2001@gmail.com>
2020-10-26 16:52:12 +0100

    Grande modification

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-from art import *
+#def say_hello(name):
+#    print("Hello " + name)
+#    print(f"How are you {name} ?")
 
-def say_hello(name):
-    print("Hello " + name)
-    print(f"How are you {name} ?")
+#n = "Kilian"
+#say_hello(n)
+#h = art("Hello")
+# print(h)
+# print(text2art("Hello",))
+# print("Yo")
+def create_contact(name, phone_number, is_favorite):
+    c = {
+        'name': name,
+        'phone_number': phone_number,
+        'is_favorite': is_favorite,
+    }
+    return c
+annuaire = {}
 
-n = "Kilian"
-say_hello(n)
-h = art("Hello")
-print(h)
-print(text2art("Hello", font='block'))
-print("Yo", font='block')
+def add_contact(contact):
+    cle = contact['phone_number']
+    annuaire[cle] = contact
+
+c = create_contact('Kilian', '0769062552', True)
+add_contact(c)
+c = create_contact('Max', '0145935203', False)
+add_contact(c)
+c = create_contact('Lucie', '0967203628', False)
+add_contact(c)
+c = create_contact('Faycal', '0783429508', True)
+add_contact(c)
+print(annuaire)
+
+def get_names():
+    names = []
+    for phone_number in annuaire:
+        contact = annuaire[phone_number]
+        names.append(contact['name'])
+    names.sort()
+    return names
+
+print(get_names())

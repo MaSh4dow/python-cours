8e954b215aa1a32e592b34fcc58e113df86e8810
Shadow <kcoudray2001@gmail.com>
2020-10-26 11:22:24 +0100

    Premier commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from art import *
+
+def say_hello(name):
+    print("Hello " + name)
+    print(f"How are you {name} ?")
+
+n = "Kilian"
+say_hello(n)
+h = art("Hello")
+print(h)
+print(text2art("Hello", font='block'))
